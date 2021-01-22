326ec4bd46561cb0eef79a7b533ebce73e9bee78
JBurtonBarr <67210834+JBurtonBarr@users.noreply.github.com>
2021-01-22 00:32:27 +0000

    Adding layers

--- Band_Play.py
+++ Band_Play.py
@@ -1,5 +1,6 @@
 #Neural Network Band Test
 import tensorflow as tf
+import matplotlib as plt
 
 num_rec = tf.keras.datasets.mnist #Dataset including handwritten numbers 0-9
  # Stick to these now and upgrade the system later
@@ -9,3 +10,22 @@ num_rec = tf.keras.datasets.mnist #Dataset including handwritten numbers 0-9
       # e.g. 2     7       = 14 = N
 
 (x_train, y_train), (x_test, y_test) = num_rec.load_data()
+
+
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
+# Normalisation helps the network learn
+
+plt.imshow(x_train[0])
+plt.show()
+
+model = tf.keras.models.Sequential()
+   #Feed forward model
+
+model.add(tf.keras.layers.Flatten())
+  #Clean up mess
+
+model.add(tf.keras.layers.Dense(256, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(256, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
+   # output layer = number of possible outcomes 
