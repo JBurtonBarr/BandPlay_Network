29373d6712c8c8623c78e93dbd12c3757dcb15d5
JBurtonBarr <67210834+JBurtonBarr@users.noreply.github.com>
2021-01-23 01:46:32 +0000

    Update Band_Play_Overlay.py

--- Band_Play_Overlay.py
+++ Band_Play_Overlay.py
@@ -1,5 +1,7 @@
 import Band_Play as bp
+import webbrowser
 
+#MAKE THIS CASE INSENSITIVE
 
 prediction = bp.run_model().get_output() #should be a list of lists
 
@@ -16,11 +18,17 @@ def letter_map(letter_sum):
 
 
 
-the_lineUp = ["ACDC", "Pink", "Joji", "Kermy", "Dappy", "ABBA"]
+the_lineUp = {"ACDC":"https://youtu.be/v2AC41dglnM", "Pink":"https://youtu.be/FJfFZqTlWrQ",
+              "Joji":"https://youtu.be/K3Qzzggn--s", "Kermy":"https://youtu.be/WS3Lkc6Gzlk",
+              "Dappy":"https://youtu.be/WoImizvsj5w", "ABBA":"https://youtu.be/xFrGuyw1V8s"}
+
+lineUp_num = {"ACDC":, "Pink":, "Joji":, "Kermy":, "Dappy":, "ABBA":}
+       #ADD letter numerical equivilents
 
 
 band = ""
 band_list = []
+code_hold = 0
 
 
 for item1 in prediction: #REMEMBER TO AUTO FILL IF A NUMBER IS NOT PRE N2
@@ -29,4 +37,17 @@ for item1 in prediction: #REMEMBER TO AUTO FILL IF A NUMBER IS NOT PRE N2
     band += letter_hold # raw string
     band_list.append(letter_hold) #add letter to list 
     
+
+if band in the_lineUp:
+    song_link = the_lineUp.get(band, default = "https://youtu.be/FxYw0XPEoKE") # default is just a fun error solution
+    webbrowser.open(song_link) #try... needs network checks etc
+else if band not in the_lineUp:
+    for item in band_list:
+        letnum = ord(num)
+        code_hold += letnum
+
+    #Add code here that goes to nearest band value if value not matched 
+
+
+
     
